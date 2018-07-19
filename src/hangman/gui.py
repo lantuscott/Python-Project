56a9b29d47ba15d366c9a551292a54602b59ed0a
leonardo farinha <leonardofarinhap@gmail.com>
2018-07-19 16:58:27 -0500

    Added gui changes

--- src/hangman/gui.py
+++ src/hangman/gui.py
@@ -1,8 +1,8 @@
 #
 #  This script contains the GUI management for the game.
 #
-from tkinter import Tk, Label, Button, Frame, Entry, StringVar
-
+from tkinter import Tk, Label, Button, Frame, Entry, StringVar, Canvas
+import turtle
 
 class Hangman:
 
@@ -10,32 +10,36 @@ class Hangman:
     This class creates a list of words and does different things to it like returning the list
     """
 
-    def __init__(self, canvas):
-        self.canvas = canvas
-        canvas.title("Hangman, The NEWER")
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Hangman, The NEWER")
 
-        self.frame = Frame(canvas, width=800, height=600)
-        self.frame.pack()
+        self.canvas = Canvas(master=self.root, width=800, height=600)
+        self.canvas.pack()
+
+        leo = turtle.RawTurtle(self.canvas)
+        leo.shape("turtle")
+        leo.forward(100)
 
         letter = StringVar()
-        self.word = Entry(canvas, textvariable=letter, width=2)
+        self.word = Entry(self.root, textvariable=letter, width=2)
         self.word.pack()
 
-        self.label = Label(canvas, text="This is our first GUI!")
+        self.label = Label(self.root, text="This is our first GUI!")
         self.label.pack()
 
-        self.greet_button = Button(canvas, text="Greet", command=self.greet)
+        self.greet_button = Button(self.root, text="Greet", command=self.greet)
         self.greet_button.pack()
 
-        self.close_button = Button(canvas, text="Close", command=canvas.quit)
+        self.close_button = Button(self.root, text="Close", command=self.root.quit)
         self.close_button.pack()
+
+        self.root.mainloop()
 
     def greet(self):
         print("Greetings!")
 
-
-root = Tk()
-canvas = Hangman(root)
-root.mainloop()
+# creates and object and starts the game
+game = Hangman()
 
 
