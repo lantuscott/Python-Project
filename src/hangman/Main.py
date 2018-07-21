00f90493c972810d06dbfa3521692c527a304d72
leonardo farinha <leonardofarinhap@gmail.com>
2018-07-21 11:31:18 -0500

    Auto stash before merge of "MariBranch" and "origin/MariBranch"

--- src/hangman/Main.py
+++ src/hangman/Main.py
@@ -4,10 +4,16 @@
 def main():
     import src.hangman.Word as Word
     import src.hangman.Drawer as Drawer
+<<<<<<< Updated upstream
     guivaina = Drawer.Drawer()
     guivaina.game_screen()
     # gameStart = Word.Word()
     # gameStart.game_logic()
+=======
+    gameStart = Word.Word()
+    gameStart.game_logic()
+
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     main()
--- src/hangman/Word.py
+++ src/hangman/Word.py
@@ -7,8 +7,8 @@ import random
 
 acc = 0
 
-class Word:
 
+class Word:
     """
     This class reads words from a txt file, creates a list of words and returns it
     """
@@ -29,8 +29,8 @@ class Word:
     """
 
     def read_words_from_file(self):
-        path_to_utils = self.path_to_project+"/utils"
-        words = open(path_to_utils+'/words.txt', 'r')
+        path_to_utils = self.path_to_project + "/utils"
+        words = open(path_to_utils + '/words.txt', 'r')
         self.my_words = words.read().split()
         words.close()
 
@@ -46,39 +46,50 @@ class Word:
     self.method
     self.get_my_words()
     '''
-    def random_word(self):      # selects a word from dictionary to
+
+    def random_word(self):  # selects a word from dictionary to
         #  start game
         ranWord = self.get_my_words()[random.randint(0, 99)]
         return ranWord
 
-# Checks if letter is in mystery word and tracks number of guesses made and remaining
+    # Checks if letter is in mystery word and tracks number of guesses made and remaining
     def game_logic(self):
-        display_ranWord = self.random_word()
-        print(display_ranWord)
-        attempts = print("You have", len(display_ranWord)*2, "attempts")
+        original_word_to_guess = self.random_word()
+        mystery_word = self.mystery_word(original_word_to_guess)
         acc = 0
         guessed_letters = ''
         progress = ''
-
+        word_to_list = []
+        temp = ""
+        remaining_letters = original_word_to_guess
+        word_to_list = list(mystery_word)
+        print("The words is :", original_word_to_guess)
         # User input to guess letter
         while True:
+            print("Starred word: ", temp)
             guess = input("Enter a letter: ")
             guess = guess.lower()  # converts input to uppercase
 
             # Displays how many guesses you have made
             for attempts in range(1):
-                attempts = len(display_ranWord)*2
+                attempts = len(original_word_to_guess) * 2
                 acc += 1
                 print("Attempt #:", acc)
                 attempts = attempts - acc
+                word_to_display = original_word_to_guess;
+
                 # Checks if input is valid (a single letter or if the input is same as length of the mystery word)
                 if guess in "abcdefghijklmnopqrstuvwxyz":
                     if len(guess) == 1:
                         progress += guess
                         # print("You guessed: ", guess)
-                        if guess in display_ranWord:
-                            print("you guessed one letter!")
-                        else: # Here call the Turtle and create a figure every time a user misses
+                        if guess in remaining_letters:
+                            remaining_letters = remaining_letters.replace(guess, '')
+                            guessed_position = word_to_display.index(guess)
+                            word_to_list[guessed_position] = guess
+                            temp = "".join(word_to_list)
+                            print("you guessed one letter!, remaining letters to be guessed: ", remaining_letters)
+                        else:  # Here call the Turtle and create a figure every time a user misses
                             print("try again")
                 # If input is not valid prints error
                 elif guess not in "abcdefghijklmnopqrstuvwxyz":
@@ -90,9 +101,15 @@ class Word:
                 print("Letters you have tried: ", guessed_letters)
 
                 # When you guess the word, it will compare it with your progress, if it's a match the user wins
-                if progress == display_ranWord and attempts > 0:
+                if remaining_letters == "" and attempts > 0:
                     print("You guessed the word! HOORAY!!!")
                     return 1
-                elif attempts <= 0: # if attempts reach out the max, the user loses
+                elif attempts <= 0:  # if attempts reach out the max, the user loses
                     print("Game Over!")
-                    return -1  #  I use this to exit the While loop
+                    return -1  # I use this to exit the While loop
+
+    def mystery_word(self, mystery_word):
+        stars = ""
+        for x in range(len(mystery_word)):
+            stars += "*"
+        return stars
