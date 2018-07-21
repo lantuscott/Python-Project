01c2c9b30c8220c10d0ab520a4b57ca24d83e919
leonardo farinha <leonardofarinhap@gmail.com>
2018-07-21 11:42:20 -0500

    close to done

--- src/hangman/Main.py
+++ src/hangman/Main.py
@@ -4,16 +4,12 @@
 def main():
     import src.hangman.Word as Word
     import src.hangman.Drawer as Drawer
-<<<<<<< Updated upstream
-    guivaina = Drawer.Drawer()
-    guivaina.game_screen()
-    # gameStart = Word.Word()
-    # gameStart.game_logic()
-=======
+
+    # guivaina = Drawer.Drawer()
+    # guivaina.game_screen()
     gameStart = Word.Word()
     gameStart.game_logic()
 
->>>>>>> Stashed changes
 
 if __name__ == '__main__':
     main()
